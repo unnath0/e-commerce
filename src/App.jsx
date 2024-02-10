71e20ced52f129adf0ca7bc245ceb8932c862a59
unnath0 <timeandwind01@gmail.com>
2024-02-10 16:34:38 +0530

    Done with home page. Components: Footer,Item,NewCollections,NewsLetter,Offers,Popular. Pages: Shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar/Navbar";
-import Shop from "./pages/Shop";
-import ShopCategory from "./pages/ShopCategory";
-import Product from "./pages/Product"
-import Cart from "./pages/Cart"
-import LoginSignup from "./pages/LoginSignup"
+import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product"
+import Cart from "./Pages/Cart"
+import LoginSignup from "./Pages/LoginSignup"
+import Footer from "./Components/Footer/Footer";
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
         <Route path="/cart" element={<Cart/>}/>
         <Route path="/login" element={<LoginSignup/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
